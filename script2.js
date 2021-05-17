16dd3d21166684d43f165d9850e145373fd744af
Lalit <lalit.garghate@gmail.com>
2021-05-17 16:56:35 +0530

    getting data from json

---
+++ script2.js
@@ -0,0 +1,24 @@
+const svg = d3.select('svg');
+console.log(svg);
+
+d3.json('circles.json').then(data=>{
+    
+    // join with data
+    const circle = svg.selectAll('circle')
+        .data(data);
+    
+    // add attr to already present circle in dome
+    circle.attr('cy', 100)
+        .attr('cx',d=>d.distance)
+        .attr('r', d=>d.radius)
+        .attr('fill', d=>d.color)
+
+    // append enter selection to dom
+    circle.enter()
+        .append('circle')
+        .attr('cy', 100)
+        .attr('cx',d=>d.distance)
+        .attr('r', d=>d.radius)
+        .attr('fill', d=>d.color)
+
+})
