8b50f4588071fbef4582b9882aea802232c29ecb
Lalit <lalit.garghate@gmail.com>
2021-05-17 14:26:35 +0530

    attr method

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const canvas = d3.select(".canvas");
-const svg = canvas.append('svg');
-svg.append('rect');
-svg.append('circle');
-svg.append('line');
+
+const svg = canvas.append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
+
+svg.append('rect')
+    .attr('height', 70)
+    .attr('width', 100)
+    .attr('x', 50)
+    .attr('y', 50)
+    .attr('fill', 'blue');
+
+
+svg.append('circle')
+    .attr('r', 70)
+    .attr('cx', 250)
+    .attr('cy', 90)
+    .attr('fill','pink');
+
+svg.append('line')
+    .attr('x1', 400)
+    .attr('x2', 500)
+    .attr('y1', 50)
+    .attr('y2', 150)
+    .attr('stroke', 'red');
