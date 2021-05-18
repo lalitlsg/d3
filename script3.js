975d144bedc19388b65391c7f1ffaa97d1892261
Lalit <lalit.garghate@gmail.com>
2021-05-18 07:38:37 +0530

    creating axis

--- script3.js
+++ script3.js
@@ -5,22 +5,28 @@ const svg = d3.select('.canvas')
     .attr('width', 600)
     .attr('height', 600);
 
-//create margins and dimensions
+// create margins and dimensions
 const margin = {top: 20, right:20, bottom: 100, left: 100}
 const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
+// create group for whole graph
 const graph = svg.append('g')
     .attr('width', graphWidth)
     .attr('height', graphHeight)
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+// create groups for x and y axis
+const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0,${graphHeight})`);
+const yAxisGroup = graph.append('g');
 
 d3.json('orders.json').then(data=>{
     
     // linear scale
     const y = d3.scaleLinear()
         .domain([0, d3.max(data, d=>d.quantity)])
-        .range([0,500]);
+        .range([0,graphHeight]);
 
     // band scale
     const x = d3.scaleBand()
@@ -52,4 +58,11 @@ d3.json('orders.json').then(data=>{
         .attr('height',d=>y(d.quantity))
         .attr('x', d=>x(d.name))
         .attr('fill', 'blue');
+
+    // create and call the axis
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
 })
