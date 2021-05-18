df8458ab41506341bbb8fe20a4a4f2e5dcc0755c
Lalit <lalit.garghate@gmail.com>
2021-05-18 08:12:17 +0530

    tick and text formatting

--- script3.js
+++ script3.js
@@ -63,8 +63,24 @@ d3.json('orders.json').then(data=>{
 
     // create and call the axis
     const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y);
+    const yAxis = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat(d=>d + ' orders');
 
     xAxisGroup.call(xAxis);
     yAxisGroup.call(yAxis);
+
+    xAxisGroup.selectAll('text')
+        .attr('transform', 'rotate(-40)')
+        .attr('text-anchor', 'end')
+        .attr('fill', 'grey');
+    
+    xAxisGroup.select('path')
+        .attr('stroke', 'grey');
+
+    yAxisGroup.selectAll('text')
+        .attr('fill', 'grey');
+    
+    yAxisGroup.select('path')
+        .attr('stroke', 'grey');
 })
