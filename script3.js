60f8fc4d0c9440cf3b981dd5af3d1220d8cf0fa8
Lalit <lalit.garghate@gmail.com>
2021-05-18 12:41:50 +0530

    listening to realtime data

--- script3.js
+++ script3.js
@@ -1,86 +1,145 @@
 // d3 scales
 
-const svg = d3.select('.canvas')
-    .append('svg')
-    .attr('width', 600)
-    .attr('height', 600);
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", 600)
+  .attr("height", 600);
 
 // create margins and dimensions
-const margin = {top: 20, right:20, bottom: 100, left: 100}
+const margin = { top: 20, right: 20, bottom: 100, left: 100 };
 const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
 // create group for whole graph
-const graph = svg.append('g')
-    .attr('width', graphWidth)
-    .attr('height', graphHeight)
-    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+const graph = svg
+  .append("g")
+  .attr("width", graphWidth)
+  .attr("height", graphHeight)
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // create groups for x and y axis
-const xAxisGroup = graph.append('g')
-    .attr('transform', `translate(0,${graphHeight})`);
-const yAxisGroup = graph.append('g');
+const xAxisGroup = graph
+  .append("g")
+  .attr("transform", `translate(0,${graphHeight})`);
+const yAxisGroup = graph.append("g");
 
-d3.json('orders.json').then(data=>{
-    
-    // linear scale
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d=>d.quantity)])
-        .range([graphHeight, 0]);
+// linear scale
+const y = d3.scaleLinear().range([graphHeight, 0]);
 
-    // band scale
-    const x = d3.scaleBand()
-        .domain(data.map(d=>d.name))
-        .range([0,500])
-        .paddingInner(0.2)
-        .paddingOuter(0.2);
+// band scale
+const x = d3.scaleBand().range([0, 500]).paddingInner(0.2).paddingOuter(0.2);
 
-    // min, max, extent
-    // const min = d3.min(data, d=>d.quantity);
-    // const max = d3.max(data, d=>d.quantity);
-    // const extent = d3.extent(data, d=>d.quantity);
-    // console.log(min, max, extent);
+// create axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3
+  .axisLeft(y)
+  .ticks(3)
+  .tickFormat((d) => d + " orders");
 
-    // join data
-    const rect = graph.selectAll('rect')
-        .data(data);
+// axes formatting
+xAxisGroup
+  .selectAll("text")
+  .attr("transform", "rotate(-40)")
+  .attr("text-anchor", "end")
+  .attr("fill", "grey");
 
-    // add attr to dom rect
-    rect.attr('width', x.bandwidth)
-        .attr('height',d=>graphHeight- y(d.quantity))
-        .attr('x', d=>x(d.name))
-        .attr('y', d=>y(d.quantity))
-        .attr('fill', 'blue');
+xAxisGroup.select("path").attr("stroke", "grey");
 
-    // append enter selection to dom
-    rect.enter()
-        .append('rect')
-        .attr('width', x.bandwidth)
-        .attr('height',d=>graphHeight - y(d.quantity))
-        .attr('x', d=>x(d.name))
-        .attr('y', d=>y(d.quantity))
-        .attr('fill', 'blue');
+yAxisGroup.selectAll("text").attr("fill", "grey");
 
-    // create and call the axis
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y)
-        .ticks(3)
-        .tickFormat(d=>d + ' orders');
+yAxisGroup.select("path").attr("stroke", "grey");
 
-    xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis);
+// d3 update pattern
+const update = (data) => {
+  // update scales
+  y.domain([0, d3.max(data, (d) => d.quantity)]);
+  x.domain(data.map((d) => d.name));
 
-    xAxisGroup.selectAll('text')
-        .attr('transform', 'rotate(-40)')
-        .attr('text-anchor', 'end')
-        .attr('fill', 'grey');
-    
-    xAxisGroup.select('path')
-        .attr('stroke', 'grey');
+  // join data
+  const rect = graph.selectAll("rect").data(data);
 
-    yAxisGroup.selectAll('text')
-        .attr('fill', 'grey');
-    
-    yAxisGroup.select('path')
-        .attr('stroke', 'grey');
-})
+  // remove exit selection
+  rect.exit().remove();
+
+  // add attr to dom rect
+  rect
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => graphHeight - y(d.quantity))
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.quantity))
+    .attr("fill", "blue");
+
+  // append enter selection to dom
+  rect
+    .enter()
+    .append("rect")
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => graphHeight - y(d.quantity))
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.quantity))
+    .attr("fill", "blue");
+
+  // call axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+};
+
+// get data from firestore
+// db.collection("dishes")
+//   .get()
+//   .then((res) => {
+// console.log(res.docs);
+// const data = [];
+// res.docs.forEach((doc) => {
+// console.log(doc.data());
+//   data.push(doc.data());
+// });
+
+// update(data);
+
+//    update data after certain interval
+//    d3.interval(()=>{
+//        data[4].quantity += 10;
+//        update(data);
+//    },1000)
+
+// remove rect from dom
+// d3.interval(() => {
+//   data.pop();
+//   update(data);
+// }, 3000);
+//   });
+
+// get realtime data from firestore
+let data = [];
+
+db.collection("dishes").onSnapshot((res) => {
+  res.docChanges().forEach((change) => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+    // console.log(doc, change.type);
+
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const index = data.findIndex((item) => item.id === doc.id);
+        data[index] = doc;
+        break;
+      case "removed":
+        data = data.filter((item) => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+
+    update(data);
+  });
+});
+
+// d3 min, max, extent methods
+// const min = d3.min(data, d=>d.quantity);
+// const max = d3.max(data, d=>d.quantity);
+// const extent = d3.extent(data, d=>d.quantity);
+// console.log(min, max, extent);
